38da85b84528ef8144a7fce33b15a5447d621580
malli24 <malleswari82006@gmail.com>
2024-12-11 14:06:37 +0530

    Add files via upload

---
+++ access_nondef.java
@@ -0,0 +1,12 @@
+class access_nondef
+{
+   public static String s()
+   {  
+  return ("mallichaithu");
+   }
+   public static void main(String[]args)
+   {
+   String res=s();
+    System.out.println(res);
+   }
+}
