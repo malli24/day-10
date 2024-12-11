7f952ba09877d1a19856869e3f49d70fc93028a2
malli24 <malleswari82006@gmail.com>
2024-12-11 14:09:17 +0530

    Add files via upload

---
+++ access_nonpub.java
@@ -0,0 +1,12 @@
+class access_nonpub
+{
+   public static String s()
+   {  
+  return ("malliakshay");
+   }
+   public static void main(String[]args)
+   {
+   String res=s();
+    System.out.println(res);
+   }
+}
