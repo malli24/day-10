a0db037e5c6cabed0e1a19f3ac65ea1ca8fe09b4
malli24 <malleswari82006@gmail.com>
2024-12-11 14:07:03 +0530

    Add files via upload

---
+++ access_nonmod.java
@@ -0,0 +1,12 @@
+class access_nonmod
+{
+   public static String s()
+   {  
+  return ("mallianu");
+   }
+   public static void main(String[]args)
+   {
+   String res=s();
+    System.out.println(res);
+   }
+}
