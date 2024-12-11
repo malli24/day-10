3b5c53d6c59d453ff870448cd40c0aaaf2d4e311
malli24 <malleswari82006@gmail.com>
2024-12-11 14:09:01 +0530

    Add files via upload

---
+++ access_nonpro.java
@@ -0,0 +1,12 @@
+class access_nonpro
+{
+   public static String s()
+   {  
+  return ("malligops");
+   }
+   public static void main(String[]args)
+   {
+   String res=s();
+    System.out.println(res);
+   }
+}
